da45d771707a5ac863a611983dfe088e87a07ef2
k_john_gough_cp <john@SoftwareAutomata.com>
2009-12-14 02:52:23 +0000

    Move to version 1.3.6

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.3.5.191")]
-[assembly: AssemblyFileVersion("1.3.5.191")]
+[assembly: AssemblyVersion("1.3.6")]
+[assembly: AssemblyFileVersion("1.3.6")]
